f2b131fdcda17f35bf2f60e582ac9184dbd7723c
abdulazizAlsumari <az.alsumari@gmail.com>
2020-01-15 04:43:30 -0500

    add sweet alart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
+
+// // CommonJS
+// const Swal = require('sweetalert2')
 
 function App() {
   return (
@@ -9,8 +14,14 @@ function App() {
         <img className="logo" src={logo} />
         <p className="title">card buetfiy</p>
         <div className="button">
-          <button className="red">red</button>
-          <button className="green">green</button></div>
+          <button className="red" onClick={Swal.fire('you click red')}>red</button>
+          <button className="green" onClick={Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Your work has been saved',
+  showConfirmButton: false,
+  timer: 1500
+})}>green</button></div>
       </div>
     </div>
   );
